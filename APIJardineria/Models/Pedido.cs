fcab0a0dd9cc034749d1618200c66f79a21fadb1
Antonio Rodríguez <antonio@iessanvicente.com>
2020-09-23 16:43:48 +0200

    Agregar archivos de proyecto.

---
+++ APIJardineria/Models/Pedido.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIJardineria.Models
+{
+    public class Pedido
+    {
+        [Key]
+        public int Codigo_Pedido { get; set; }
+        public DateTime Fecha_Pedido { get; set; }
+        public DateTime Fecha_Esperada { get; set; }
+        public DateTime? Fecha_Entrega { get; set; }
+        public string Estado { get; set; }
+        public string Comentarios { get; set; }
+
+        [ForeignKey("cliente")]
+        public int Codigo_Cliente { get; set; }
+        
+    }
+}
---
+++ APIJardineria/Models/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIJardineria.Models
+{
+    public class Producto
+    {
+        [Key]
+        public string Codigo_Producto { get; set; }
+        public string Nombre { get; set; }
+
+        [ForeignKey("gama_productos")]
+        public string Gama { get; set; }
+ 
+        public string Dimensiones { get; set; }
+        public string Proveedor { get; set; }
+        public string Descripción { get; set; }
+        public int Cantidad_en_Stock { get; set; }
+        public decimal Precio_venta { get; set; }
+        public decimal? Precio_proveedor { get; set; }
+        
+    }
+}
